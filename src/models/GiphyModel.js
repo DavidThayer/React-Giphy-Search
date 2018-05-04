0620bb981745ed0d781fd0218bb269e0742a3b72
David Thayer <david.thayer@gmail.com>
2018-05-04 14:27:05 -0700

    gitignore update

--- src/models/GiphyModel.js
+++ src/models/GiphyModel.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-const API_KEY = 'vYTVrbmf6yTatvG6l2qnvHjWYLa6tTXv';
+const keys = require('../config/key_api');
 
 class GiphyModel {
   static search = (query) => {
     console.log("Model Query = ", query)
-    const request = axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`);
+    const request = axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${keys.api_key}`);
     return request;
   }
 
   static searchById = (id) => {
 
-    const request = axios.get(`https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`);
+    const request = axios.get(`https://api.giphy.com/v1/gifs/${id}?api_key=${keys.api_key}`);
     return request;
   }
 }
